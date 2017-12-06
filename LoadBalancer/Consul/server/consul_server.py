b2fc927f35f1f8f214eb62e43f738662161a39f8
jerrin <jerrinsuresh@gmail.com>
2017-12-05 22:38:36 -0500

    BugFix Hostname

--- LoadBalancer/Consul/server/consul_server.py
+++ LoadBalancer/Consul/server/consul_server.py
@@ -5,10 +5,11 @@ import subprocess
 if __name__ == '__main__':
         bashCommand = "ifconfig eth0 | grep -w inet | awk {'print $2'}"
         privateIP = subprocess.check_output(['bash','-c',bashCommand]).rstrip()
+        hostname = subprocess.check_output(['bash','-c','hostname']).rstrip()
         with open('/home/consul/serverlist.txt') as reader:
                 servers = reader.readlines()
                 servers = [x.replace('\n',':8301') for x in servers]
-
+    
         with open('/etc/consul.d/server/data.json','w') as outfile:
                 json.dump({
                 "client_addr":"0.0.0.0",
@@ -18,7 +19,7 @@ if __name__ == '__main__':
                 "log_level": "INFO",
                 "enable_syslog": bool(1),
                 "enable_debug": bool(1),
-                "node_name": "ConsulServer1",
+                "node_name": hostname,
                 "server": bool(1),
                 "bootstrap_expect":3,
                 "leave_on_terminate": bool(0),
