2c75e42df5569c96b0f2aa1b1e196d94189a3b5d
Nayeemullah Baig <nayeemullahbaig.93@gmail.com>
2020-06-19 12:51:41 -0400

    templates and methods

---
+++ django_airavata/apps/admin/static/django_airavata_admin/src/components/emails/SendEmailEditor.vue
@@ -0,0 +1,222 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <slot name="title">
+      </slot>
+    </div>
+    <b-form
+      @input="onUserInput"
+      novalidate
+    >
+      <b-form-group
+        label="Email Subject"
+        label-for="email-subject"
+        :invalid-feedback="getValidationFeedback('emailSubject')"
+        :state="getValidationState('emailSubject')"
+      >
+        <b-form-input
+          id="email-subject"
+          type="text"
+          v-model="data.title"
+          required
+          placeholder="Email Subject"
+          :state="getValidationState('emailSubject')"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        label="Email Body"
+        label-for="email-body"
+        :invalid-feedback="getValidationFeedback('emailBody')"
+        :state="getValidationState('emailBody')"
+      >
+        <b-form-textarea
+          id="email-body"
+          type="text"
+          v-model="data.notificationMessage"
+          required
+          placeholder="Email Body"
+          :state="getValidationState('emailBody')"
+          :rows="3"
+        ></b-form-textarea>
+      </b-form-group>
+
+      <b-form-group
+        label="Publish Date"
+        label-for="publish-date"
+      >
+        <datetime
+          type="datetime"
+          v-model="inputPublishedTime"
+          input-class="my-class"
+          value-zone="UTC"
+          :format="{ year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'short' }"
+          :phrases="{ok: 'Continue', cancel: 'Exit'}"
+          :hour-step="1"
+          :minute-step="5"
+          :min-datetime="today"
+          :week-start="7"
+          use12-hour
+          auto
+        ></datetime>
+      </b-form-group>
+
+      <b-form-group
+        label="Expiration Date"
+        label-for="expiration-date"
+      >
+        <datetime
+          type="datetime"
+          v-model="inputExpirationTime"
+          input-class="my-class"
+          value-zone="UTC"
+          :format="{ year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'short' }"
+          :phrases="{ok: 'Continue', cancel: 'Exit'}"
+          :hour-step="1"
+          :minute-step="5"
+          :min-datetime="inputPublishedTime"
+          :week-start="7"
+          use12-hour
+          auto
+
+        ></datetime>
+      </b-form-group>
+
+      <b-form-group
+        label="Priority"
+        label-for="priority"
+        :invalid-feedback="getValidationFeedback('priority')"
+        :state="getValidationState('priority')"
+      >
+        <b-form-select
+        id = "priority"
+        v-model="data.priority"
+        :options="select.options"
+        :state="getValidationState('priority')"
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <template v-if=!editEmail name="buttons">
+        <div class="row">
+          <div
+            id="col-exp-buttons"
+            class="col"
+          >
+            <b-button
+              variant="success"
+              @click="saveNewNotice"
+              :disabled="isSaveDisabled"
+            >
+              Save
+            </b-button>
+            <b-button
+              variant="primary"
+              @click="cancelNewEmail"
+            >
+              Cancel
+            </b-button>
+          </div>
+        </div>
+      </template>
+    </b-form>
+  </div>
+</template>
+<style>
+.my-class {
+  width: 250px;
+}
+</style>
+<script>
+import { models } from "django-airavata-api";
+import { mixins, utils } from "django-airavata-common-ui";
+import { Datetime } from 'vue-datetime';
+import moment from "moment";
+import 'vue-datetime/dist/vue-datetime.css'
+
+export default {
+  name: "email-editor",
+  components: {
+    datetime: Datetime
+  },
+  mixins: [mixins.VModelMixin],
+  props: {
+    value: {
+      type: models.Email,
+      required: true
+    }
+  },
+  created() {
+    //checks whether the component is used for editing or updating the email
+    if(this.value.emailID != null){
+      this.editEmail = true;
+      this.inputPublishedTime = new moment(this.value.publishedTime.toISOString()).utc().format()
+      this.inputExpirationTime = new moment(this.value.expirationTime.toISOString()).utc().format()
+      this.data.priority = this.value.priority.name;
+      this.today = new moment(this.value.expirationTime.toISOString()).format();
+    }
+  },
+  data() {
+    return {
+      editEmail: false,
+      userBeginsInput: false,
+      inputPublishedTime: null,
+      inputExpirationTime: null,
+      today: new moment().format(),
+      select : {
+          selected: "LOW",
+          options: [
+              { text: "LOW", value: "LOW" },
+              { text: "NORMAL", value: "NORMAL" },
+              { text: "HIGH", value: "HIGH" }
+            ]
+      }
+    };
+  },
+  computed: {
+    valid: function() {
+      const validation = this.data.validate();
+      return (
+        Object.keys(validation).length === 0
+      );
+    },
+    isSaveDisabled: function() {
+      return !this.valid;
+    }
+  },
+  methods: {
+    onUserInput() {
+      this.userBeginsInput = true;
+      return this.$emit("userBeginsInput");
+    },
+    reset() {
+      this.userBeginsInput = false;
+    },
+    getValidationFeedback: function(properties) {
+      return utils.getProperty(this.data.validate(), properties);
+    },
+    getValidationState: function(properties) {
+      if (this.userBeginsInput == false){
+        return null;
+      }
+      return this.getValidationFeedback(properties) ? "invalid" : "valid";
+    },
+    cancelNewEmail() {
+      return this.$emit("cancelNewEmail");
+    },
+    saveNewEmail() {
+      return this.$emit("saveNewEmail");
+    }
+  },
+  watch: {
+    inputExpirationTime() {
+      this.data.expirationTime = this.inputExpirationTime;
+
+    },
+    inputPublishedTime() {
+      this.data.publishedTime = this.inputPublishedTime;
+    }
+
+  }
+};
+</script>
---
+++ django_airavata/apps/admin/static/django_airavata_admin/src/components/emails/SendEmailManagementContainer.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h1 class="h4 mb-4">Manage Emails</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <list-layout @add-new-item="addNewEmail" title="Email"
+              new-item-button-text="New Email" :new-button-disabled="!isGatewayAdmin">
+              <template slot="new-item-editor">
+                <b-card v-if="showNewItemEditor">
+                  <email-editor
+                    v-model="newEmail"
+                    ref="emailEditor"
+                    @cancelNewEmail="cancelNewEmail"
+                    @saveNewEmail="saveNewEmail"
+                  >
+                  <template slot="title">
+                    <h1 class="h4 mb-4 mr-auto">
+                      New Email
+                    </h1>
+                  </template>
+                  </email-editor>
+                </b-card>
+              </template>
+              <template slot="item-list" slot-scope="slotProps">
+                <b-table hover :fields="fields" :items="items">
+                  <template slot="publishedTime" slot-scope="data">
+                    <human-date :date="data.value"/>
+                  </template>row
+                  <template slot="expirationTime" slot-scope="data">
+                    <human-date :date="data.value"/>
+                  </template>
+                  <template slot="action" slot-scope="data">
+                    <template v-if="data.item.userHasWriteAccess">
+                      <b-link class="action-link" @click="toggleDetails(data)">
+                        Edit
+                        <i class="fa fa-edit" aria-hidden="true"></i>
+                      </b-link>
+                      <delete-link @delete="deleteEmail(data.item.emailId)">
+                        Are you sure you want to delete the email?
+                      </delete-link>
+                    </template>
+                  </template>
+                  <template slot="row-details" slot-scope="row">
+                    <b-card>
+                      <email-editor :value="row.item" v-model="updatedEmail" @userBeginsInput="isUserBeginInput = false">
+                        <template slot="title">
+                          <h1 class="h4 mb-4 mr-auto">
+                            Update Email
+                          </h1>
+                        </template>
+                      </email-editor>
+                      <b-button variant="success" size="sm" @click="updateEmail()" :disabled="isUserBeginInput">Send Update</b-button>
+                      <b-button variant="primary" size="sm" @click="toggleDetails(row)">Close</b-button>
+                    </b-card>
+                  </template>
+                </b-table>
+              </template>
+            </list-layout>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+</template>
+
+<script>
+import { models, services, session } from "django-airavata-api";
+import { components, layouts } from "django-airavata-common-ui";
+import SendEmailEditor from "./SendEmailEditor";
+
+
+export default {
+  name: "email-management-container",
+  data() {
+    return {
+      emails: null,
+      isUserBeginInput: true,
+      showNewItemEditor: false,
+      showingDetails: {},
+    };
+  },
+  components: {
+    'human-date': components.HumanDate,
+    "delete-link": components.DeleteLink,
+    "list-layout": layouts.ListLayout,
+    EmailEditor,
+  },
+  created() {
+    
+  },
+  computed: {
+    fields() {
+      return [
+        {
+          label: "Type",
+          key: 'type'
+        },
+        {
+          label: "Email Subject",
+          key: "emailSubject"
+        },
+        {
+          label: "Users/Groups",
+          key: "users"
+        },
+        {
+          label: "Email Body",
+          key: "emailBody"
+        },
+        {
+          label: "Publish Date",
+          key: "publishedTime"
+        },
+        {
+          label: "Expiry Date",
+          key: "expirationTime"
+        },
+        {
+          label: "Priority",
+          key: "priority.name"
+        },
+        {
+          label: "Action",
+          key: "action"
+        }
+
+      ];
+    },
+    items() {
+      return this.emails
+        ? this.emails
+        : [];
+    },
+    isGatewayAdmin() {
+      return session.Session.isGatewayAdmin;
+    }
+  },
+  methods: {
+    saveNewEmail() {
+
+    },
+    cancelNewEmail() {
+      this.showNewItemEditor = false;
+    },
+    addNewEmail() {
+
+    },
+    updateEmail(){
+
+    },
+    deleteEmail() {
+
+    },
+  }
+};
+</script>
