0ebb81140a4da056bf6d96a00dc50a16c50eb6f8
Bipin Kumar, Shubham <sbipink@iu.edu>
2022-07-24 18:53:45 -0400

    pushing dashboard file

---
+++ django_airavata/apps/admin/static/django_airavata_admin/src/components/dashboards/AdminDashboardContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h1 class="h4 mb-4">Admin Dashboard</h1>
+      </div>
+    </div> 
+  </div>
+</template>
+<script>
+import { models, services, utils } from "django-airavata-api";
+import { components } from "django-airavata-common-ui";
+import ExperimentStatisticsCard from "./ExperimentStatisticsCard";
+import ExperimentDetailsView from "./ExperimentDetailsView";
+
+import moment from "moment";
+
+export default {
+  name: "admin-dashboard-container",
+};
+</script>
